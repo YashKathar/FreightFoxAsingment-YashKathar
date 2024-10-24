1f8ef0b589b2dd7ed98869d1cc1801e4adc08524
YashKathar <yashkathar4545@gmail.com>
2024-10-25 01:10:56 +0530

    the assingment of Pincode Distence code is completed but the Google maps Api Keys ant to billing first so the task is pending

--- DynamicPDFGeneration/PinCodeDistence/src/main/java/com/frightfox/controller/RouteController.java
+++ DynamicPDFGeneration/PinCodeDistence/src/main/java/com/frightfox/controller/RouteController.java
@@ -1,5 +1,24 @@
 package com.frightfox.controller;
 
-public class RouteController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.frightfox.dto.RouteInfoDto;
+import com.frightfox.pojo.RouteInfo;
+import com.frightfox.service.IRouteInfoService;
+
+@RestController
+@RequestMapping("/api/routes")
+public class RouteController {
+	  @Autowired
+	  private IRouteInfoService routeInfoService;
+	  
+	  @PostMapping
+	  public RouteInfo createRoute(@RequestBody RouteInfoDto request) {
+	      String apiKey = "YOUR_GOOGLE_MAPS_API_KEY"; // For the key It needs payment to a google so thats the pending work only apiKey
+	      return routeInfoService.getRouteInfo(request.getFromPincode(), request.getToPincode(), apiKey);
+	  }
 }
